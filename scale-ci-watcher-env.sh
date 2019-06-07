ae8c6b8ca44886a5f944eaaa645890795a1a29be
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2019-06-07 13:03:47 -0400

    Add instructions for test setup/recreating scale-ci jobs on any jenkins
    This commit:
    - Instructions to setup jenkins locally pretty quickly and test/recreate
      the scale-jobs on the test instance using scale-ci-watcher.
    - Adds a link to the scale-ci-properties repo where we maintain the
      properties files for the OCP large scale and per sprint test runs.
    - Adds a scale-ci workflow image.

--- scale-ci-watcher-env.sh
+++ scale-ci-watcher-env.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-export JJB_CONFIG_PATH=""
-export SCALE_CI_WATCHER_REPO=""
-export SCALE_CI_WATCHER_REPO_BRANCH=""
-export SCALE_CI_WATCHER_REPO_PATH=""
-export WORKDIR=""
+# Path to the jenkins job builder config to connect to jenkins
+export JJB_CONFIG_PATH="/root/scale-ci-pipeline/config/jjb.ini"
+# Repo hosting scale-ci watcher
+export SCALE_CI_WATCHER_REPO="https://github.com/openshift-scale/scale-ci-pipeline.git"
+# branch to clone
+export SCALE_CI_WATCHER_REPO_BRANCH="master"
+# Location of the scale-ci-watcher repo
+export SCALE_CI_WATCHER_REPO_PATH="/root/scale-ci-pipline"
+# Location of the scale-ci-watcher repo
+export WORKDIR="/root/scale-ci-pipeline"
+# Whether to update the scale-ci jobs or not
 export UPDATE_SCALE_CI_JOBS=true
+# This is useful when running the watcher job in jenkins, it adds any public key to the jump host, this way other teams can access the jump host with having the perf key.
+# Whether to add public key or not
 export ADD_PUBLIC_KEY=false
+# Public key to be added to the jump host
 export PUBLIC_KEY=""
-export KEYS_PATH="" 
+# Location of the authorized keys file to add the public key
+export KEYS_PATH="/root/.ssh/authorized_keys" 
